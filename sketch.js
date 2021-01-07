6d5b3187764e04959b4f9676807b5198ecf82327
YaduvamsiB <76206851+YaduvamsiB@users.noreply.github.com>
2021-01-07 15:57:57 +0530

    added bounceOff and Library

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(600, 600);
   fixed = createSprite(300, 300, 60, 100);
-  moving = createSprite(300, 300, 40, 60);
+  moving = createSprite(20, 300, 40, 60);
+
+  moving.velocityX = 2;
+
+  rect1 = createSprite(100, 100, 40, 40);
+  rect2 = createSprite(200, 100, 40, 40);
+  rect3 = createSprite(300, 100, 40, 40);
+  rect4 = createSprite(400, 100, 40, 40);
+  rect5 = createSprite(500, 100, 40, 40);
 
   moving.shapeColor = "green";
   fixed.shapeColor = "blue";
@@ -9,16 +17,12 @@ function setup() {
 
 function draw() {
   background(rgb(137, 0, 255));
+  bounceOff(moving, fixed);
+  //moving.y = mouseY;
+  //moving.x = mouseX;
 
-  moving.y = mouseY;
-  moving.x = mouseX;
-
-  if (
-    moving.x - fixed.x < moving.width / 2 + fixed.width / 2 &&
-    fixed.x - moving.x < moving.width / 2 + fixed.width / 2 &&
-    moving.y - fixed.y < moving.width / 2 + fixed.width / 2 &&
-    fixed.y - moving.y < moving.width / 2 + fixed.width / 2
-  ) {
+  //arguments
+  if (isTouching(fixed, moving)) {
     moving.shapeColor = "black";
     fixed.shapeColor = "white";
   } else {
@@ -26,5 +30,47 @@ function draw() {
     fixed.shapeColor = "blue";
   }
 
+  if (isTouching(rect1, moving)) {
+    moving.shapeColor = "black";
+    rect1.shapeColor = "orange";
+  } else {
+    moving.shapeColor = "green";
+    rect1.shapeColor = "grey";
+  }
+
+  if (isTouching(rect2, moving)) {
+    moving.shapeColor = "black";
+    rect2.shapeColor = "yellow";
+  } else {
+    moving.shapeColor = "green";
+    rect2.shapeColor = "grey";
+  }
+
+  if (isTouching(rect3, moving)) {
+    moving.shapeColor = "black";
+    rect3.shapeColor = "white";
+  } else {
+    moving.shapeColor = "green";
+    rect3.shapeColor = "grey";
+  }
+
+  if (isTouching(rect4, moving)) {
+    moving.shapeColor = "black";
+    rect4.shapeColor = "white";
+  } else {
+    moving.shapeColor = "green";
+    rect4.shapeColor = "grey";
+  }
+
+  if (isTouching(rect5, moving)) {
+    moving.shapeColor = "black";
+    rect5.shapeColor = "white";
+  } else {
+    moving.shapeColor = "green";
+    rect5.shapeColor = "grey";
+  }
+
   drawSprites();
 }
+
+//parameters
